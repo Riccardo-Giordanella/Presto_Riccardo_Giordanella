ffefbb62115acc722b383f68c59a4c2ddc00433b
Riccardo Giordanella <it.gg@hotmail.it>
2024-10-21 19:57:10 +0200

    Crop immagine

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -4,9 +4,11 @@ namespace App\Livewire;
 
 use App\Models\Article;
 use Livewire\Component;
+use App\Jobs\ResizeImage;
 use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 
 class CreateArticleForm extends Component
 {
@@ -39,12 +41,13 @@ class CreateArticleForm extends Component
             'user_id' => Auth::id()
         ]);
 
-        if (count($this->images) > 0)
-        {
-            foreach ($this->images as $image)
-            {
-                $this->article->images()->create(['path' => $image->store('images', 'public')]);
+        if (count($this->images) > 0){
+            foreach ($this->images as $image){
+                $newFileName = "/articles/{$this->article->id}";
+                $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
+                dispatch(new ResizeImage($newImage->path, 300, 300));
             }
+            File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }
 
         session()->flash('success', 'Articolo creato correttamente');
